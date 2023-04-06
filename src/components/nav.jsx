3dc129e845e2ea655cec79b40c15b5a3d83232ab
Byiringiro Saad <byiringirosaad@gmail.com>
2023-04-06 09:20:42 +0200

    new nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,32 +1,83 @@
 import React from "react";
 import styled from "styled-components";
 
+// images
+import logo from "../assets/favicon.ico";
+
 const Nav = () => {
   return (
     <Container>
-      <p>
-        Legal-<span>AI</span>
-      </p>
+      <div className="content">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="nav">
+          <a href="#" className="link blue">
+            We're hiring!
+          </a>
+          <p className="link ">Join our Waitlist</p>
+          <a href="#" className="link last">
+            Resources
+          </a>
+        </div>
+      </div>
     </Container>
   );
 };
 
 const Container = styled.div`
-  width: 100%;
+  width: 100vw;
   height: 70px;
-  margin: 0 0 30px 0;
   display: flex;
-  flex-direction: row;
   align-items: center;
-  padding: 0 20px;
+  justify-content: center;
+  border-bottom: 1px solid var(--gray);
 
-  p {
-    font-size: 1.2rem;
-    color: var(--black);
+  .content {
+    height: 100%;
+    padding: 0 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
 
-    span {
-      font-weight: 700;
-      color: var(--blue);
+    @media only screen and (min-width: 1200px) {
+      width: 1200px;
+    }
+
+    @media only screen and (max-width: 1200px) {
+      width: 95%;
+    }
+
+    .logo {
+      width: auto;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .nav {
+      width: auto;
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      .link {
+        margin: 0 20px;
+        text-decoration: none;
+        color: var(--black);
+      }
+
+      .blue {
+        color: var(--blue);
+        text-decoration: underline;
+      }
+
+      .last {
+        margin: 0 0 0 20px !important;
+      }
     }
   }
 `;
